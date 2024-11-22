863aa11dae6d43b1c11873565c7841ebdc24da60
Srikanth Reddy PVJ <94885124+imsrikanth28@users.noreply.github.com>
2024-11-22 11:47:43 +0530

    Update WordCount.java

--- WordCount.java
+++ WordCount.java
@@ -1,3 +1,69 @@
+--------------------------------------------------------------------------------------------------
+ MAPPER CLASS:-
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WordCountMapper extends Mapper<Object, Text, Text, IntWritable> {
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        String[] words = value.toString().split("\\s+"); // Split by spaces
+        for (String w : words) {
+            word.set(w);
+            context.write(word, one); // Emit <word, 1>
+        }
+    }
+}
+--------------------------------------------------------------------------------------------------------
+REDUCER CLASS:-
+ import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        int sum = 0;
+        for (IntWritable val : values) {
+            sum += val.get(); // Sum up counts
+        }
+        context.write(key, new IntWritable(sum)); // Emit <word, total_count>
+    }
+}
+--------------------------------------------------------------------------------------------------------
+DRIVER/MAIN CODE:-
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class WordCount {
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "word count");
+        job.setJarByClass(WordCount.class);
+        job.setMapperClass(WordCountMapper.class);
+        job.setCombinerClass(WordCountReducer.class); // Optional
+        job.setReducerClass(WordCountReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0])); // Input path
+        FileOutputFormat.setOutputPath(job, new Path(args[1])); // Output path
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
+---------------------------------------------------------------------------------------------
+
+
+
+
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -47,3 +113,12 @@ InterruptedException {
  System.exit(job.waitForCompletion(true) ? 0 : 1);
  }
 }
+
+
+
+
+
+
+
+
+ 
